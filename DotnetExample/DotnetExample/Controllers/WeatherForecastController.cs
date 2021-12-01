0c4cd819092b3d0ff3628c39069757ed2d6fd754
Dan Percic <danpercic86@gmail.com>
2021-12-01 20:48:09 +0200

    added post method

--- DotnetExample/DotnetExample/Controllers/WeatherForecastController.cs
+++ DotnetExample/DotnetExample/Controllers/WeatherForecastController.cs
@@ -11,6 +11,8 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private static List<WeatherForecast> database = GenerateWf();
+
     private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -18,8 +20,21 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet]
     public IEnumerable<WeatherForecast> Get()
+    {
+        return database;
+    }
+
+    // these are attributes
+    [HttpPost]
+    public WeatherForecast Post(WeatherForecast weatherForecast)
+    {
+        database.Add(weatherForecast);
+        return database.Find(item => item == weatherForecast);
+    }
+
+    private static List<WeatherForecast> GenerateWf()
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
@@ -27,6 +42,6 @@ public class WeatherForecastController : ControllerBase
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
-           .ToArray();
+           .ToList();
     }
 }
